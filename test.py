62df492c0858f755c1dd6ba69ea7a5a916665583
8pt-cool <xin.zhangbupt@gmail.com>
2019-09-30 17:39:33 +0800

    test 2.0
    test 2.0

--- test.py
+++ test.py
@@ -25,15 +25,37 @@ r= requests.get(
     "https://mp.weixin.qq.com/s/V6LfeY6Mki8VDyFYyfud2Q"
 )
 htmls=r.text
-
+#print(htmls)
+#regex = re.compile("(>(\d{3,})\s{1,}< a href=(.*)\s{1,}target)")
+#print(regex.findall(htmls))
 
 soup =BeautifulSoup(htmls,'lxml')
+
 #ttt=soup.find_all('p',{'style':'white-space: normal;line-height: 25.6px;max-width: 100%;min-height: 1em;box-sizing: border-box !important;word-wrap: break-word !important;'})
 #ttt=soup.find_all('a',{'data-linktype':'2'})
-ttt=soup.find_all('span',{'style':'max-width: 100%;white-space: pre-wrap;font-size: 14px;box-sizing: border-box !important;word-wrap: break-word !important;'})
-for t in(ttt):
-    print(t)
+#print(soup)
+for child in soup.descendants:
+    if child.name =='p':
+        #print(child)
+        if child.a is None:
+            continue
+        else:
+            print(child.a)
+            list = []
+            for string in child.strings:
+                if len(string) >2:
+                    list.append(string)
+            print(list)
+    #regex = re.compile("(>(\d{3,})\s{1,}< a href=(.*)\s{1,}target)")
+    #print(regex.findall(t))
+
+
+
 """
+for t in(ttt):
+    print(ttt.parent())
+#    print(t)
+
 for t in ttt:
     link = t.get('href')
     print(link)
@@ -46,8 +68,8 @@ for t in ttt:
 #    print(patten)
     htmls = '<p style="font-size: 16px;line-height: 25.6000003814697px;white-space: normal;max-width: 100%;min-height: 1em;color: rgb(62, 62, 62);box-sizing: border-box !important;word-wrap: break-word !important;background-color: rgb(255, 255, 255);"><span style="max-width: 100%;white-space: pre-wrap;font-size: 14px;box-sizing: border-box !important;word-wrap: break-word !important;">001 <a href="http://mp.weixin.qq.com/s?__biz=MzIxMTMzNTc5OA==&amp;mid=2247483656&amp;idx=1&amp;sn=d7b68107b3e80d87974b02471a976b6c&amp;scene=21#wechat_redirect" target="_blank" data_ue_src="http://mp.weixin.qq.com/s?__biz=MzIxMTMzNTc5OA==&amp;mid=2247483656&amp;idx=1&amp;sn=d7b68107b3e80d87974b02471a976b6c&amp;scene=21#wechat_redirect" data-linktype="2">垫底辣妹</a>（嘉宾：冰心、龙源）</span></p>'
     print(html.unescape(htmls))
+    #number = re.findall('[0-9]{3,}%s(.+?)' %link_href,html.unescape(htmls))
     number = re.findall('[0-9]{3,}%s(.+?)' %link_href,html.unescape(htmls))
-
     print(number)
     
     url = "oreilly.com"
