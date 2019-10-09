ebcba039e863e59f77ed96bc61a2e7e49c0b76ae
Charles <xin.zhangbupt@gmail.com>
2019-10-09 23:24:50 +0800

    Finally
    add the seq finally

--- fpyp.py
+++ fpyp.py
@@ -23,56 +23,75 @@ r= requests.get(
 html=r.text
 soup =BeautifulSoup(html,'lxml')
 #ttt=soup.find_all('p',{'style':'white-space: normal;line-height: 25.6px;max-width: 100%;min-height: 1em;box-sizing: border-box !important;word-wrap: break-word !important;'})
-ttt=soup.find_all('a',{'data-linktype':'2'})
-if(1):
-    file_rss=open(rss,'w',encoding='UTF-8')
-    file_head=open(head,'r',encoding='UTF-8')
-    head_text=file_head.read()
-    file_rss.write(head_text)
-    file_head.close()
-    #audio_link=''
-    for t in reversed(ttt):
-        title=t.get_text()
-        if title == ' ':
+file_rss=open(rss,'w',encoding='UTF-8')
+file_head=open(head,'r',encoding='UTF-8')
+head_text=file_head.read()
+file_rss.write(head_text)
+file_head.close()
+child_list=[]
+for child in soup.descendants:
+    child_list.append(child)
+for child in reversed(child_list):
+    if child.name =='p':
+        #print(child)
+        if child.a is None:
             continue
-        #print(title)
-        #print(t.get('href'))
-        link = t.get('href')
-        link_request= requests.get(link)
-        link_html=link_request.text
-        # print(link_html)
-        #      print(link_html)
-        for line in link_html.splitlines():
-            #print(line)
-            if 'msg_source_url' in line:
-                #print(line)
-                if(title=='超人总动员2'):
-                    audio_link='http://image.kaolafm.net/mz/audios/201806/d96f030a-3eba-4447-9d47-0703332f07b4.mp3'
+        if not child.a.has_attr('data-linktype'):
+            continue
+        else:
+            #print(child)
+            showseq = []
+            for string in child.strings:
+                showseq.append(string)
+                break
+            print(showseq)
+            ttt=child.find_all('a',{'data-linktype':'2'})
+            for t in reversed(ttt):
+                title = t.get_text()
+                if title == ' ':
                     continue
-                audio_link=re.findall(r"\'(.+?)\'",line)[0]
-                print(audio_link)
+                title=showseq[0]+title
+                print(title)
+                # print(title)
+                # print(t.get('href'))
+                link = t.get('href')
+                link_request = requests.get(link)
+                link_html = link_request.text
+                # print(link_html)
+                #      print(link_html)
+                for line in link_html.splitlines():
+                    # print(line)
+                    if 'msg_source_url' in line:
+                        # print(line)
+                        if (title == '114 超人总动员2'):
+                            audio_link = 'http://image.kaolafm.net/mz/audios/201806/d96f030a-3eba-4447-9d47-0703332f07b4.mp3'
+                            continue
+                        audio_link = re.findall(r"\'(.+?)\'", line)[0]
+                        print(audio_link)
 
-#                audio = MP3(str(audio_link))
-#                print(audio.info.length)
-#                audiofile = AudioFileClip(url)
-            time_stamp=re.findall(r",n=\"(.+?)\"",line)
-            if(time_stamp):
-                time_local=time.localtime(int(time_stamp[0]))
-                time_local=time.strftime("%a, %d %b %Y %H:%M:%S +0800", time_local)
-                time_label='<pubDate>'+time_local+'</pubDate>'
+                    #                audio = MP3(str(audio_link))
+                    #                print(audio.info.length)
+                    #                audiofile = AudioFileClip(url)
+                    time_stamp = re.findall(r",n=\"(.+?)\"", line)
+                    if (time_stamp):
+                        time_local = time.localtime(int(time_stamp[0]))
+                        time_local = time.strftime("%a, %d %b %Y %H:%M:%S +0800", time_local)
+                        time_label = '<pubDate>' + time_local + '</pubDate>'
+    #audio_link=''
+                title_label = '<title>' + title + '</title>'
+                link_label = '<link>' + audio_link + '</link>'
+                guid_label = '<guid>' + audio_link + '</guid>'
+                enclousure_label = '<enclosure url="' + audio_link + '" type="audio/mpeg"/>'
+                itunes_author_label = '<itunes:author>波米和他的朋友们</itunes:author>'
+                itunes_subtitle_label = '<itunes:subtitle>' + title + '</itunes:subtitle>'
+                itunes_summart_label = '<itunes:summary>欢迎关注“反派影评”公众号，可听到30分钟行业类谈话节目“反派马后炮”及短语音评电影的“电影耳旁风”，另外还可获取节目中提及的电影片单及其它延展信息。</itunes:summary>'
+                itunes_image_label = ''
+                decription_label = '<description>欢迎关注“反派影评”公众号，可听到30分钟行业类谈话节目“反派马后炮”及短语音评电影的“电影耳旁风”，另外还可获取节目中提及的电影片单及其它延展信息。</description>'
+                item = '<item>' + title_label + enclousure_label + time_label + itunes_author_label + itunes_subtitle_label + itunes_summart_label + guid_label + link_label + decription_label + '</item>\n'
+                file_rss.write(item)
                 #<pubDate>Tue, 03 Jul 2018 04:23:44 GMT</pubDate>
 #<enclosure url="http://static.storyfm.cn/media/2017/07/Across_the_strait.mp3" length="26.6 MB" type="audio/mpeg"/>
-        title_label = '<title>' + title + '</title>'
-        link_label = '<link>' + audio_link + '</link>'
-        guid_label = '<guid>' + audio_link + '</guid>'
-        enclousure_label='<enclosure url="'+audio_link+'" type="audio/mpeg"/>'
-        itunes_author_label = '<itunes:author>波米和他的朋友们</itunes:author>'
-        itunes_subtitle_label = '<itunes:subtitle>' + title + '</itunes:subtitle>'
-        itunes_summart_label = '<itunes:summary>欢迎关注“反派影评”公众号，可听到30分钟行业类谈话节目“反派马后炮”及短语音评电影的“电影耳旁风”，另外还可获取节目中提及的电影片单及其它延展信息。</itunes:summary>'
-        itunes_image_label = ''
-        decription_label = '<description>欢迎关注“反派影评”公众号，可听到30分钟行业类谈话节目“反派马后炮”及短语音评电影的“电影耳旁风”，另外还可获取节目中提及的电影片单及其它延展信息。</description>'
-        item = '<item>' + title_label + enclousure_label + time_label + itunes_author_label + itunes_subtitle_label + itunes_summart_label + guid_label + link_label + decription_label + '</item>\n'
-        file_rss.write(item)
+
 #       break
 file_rss.write('</channel>\n</rss>')
 file_rss.close()
--- test.py
+++ test.py
@@ -34,7 +34,10 @@ soup =BeautifulSoup(htmls,'lxml')
 #ttt=soup.find_all('p',{'style':'white-space: normal;line-height: 25.6px;max-width: 100%;min-height: 1em;box-sizing: border-box !important;word-wrap: break-word !important;'})
 #ttt=soup.find_all('a',{'data-linktype':'2'})
 #print(soup)
+child_list=[]
 for child in soup.descendants:
+    child_list.append(child)
+for child in reversed(child_list):
     if child.name =='p':
         #print(child)
         if child.a is None:
@@ -42,20 +45,18 @@ for child in soup.descendants:
         if not child.a.has_attr('data-linktype'):
             continue
         else:
-            print(child)
-            ttt=child.find_all('a',{'data-linktype':'2'})
-            for t in ttt:
-                print(t)
-            showlist = []
+            #print(child)
+            showseq = []
             for string in child.strings:
-                if(string == ' ' or string == '"' or string =='&' or string =='“' or string == '”' or string == '（' or string =='）'):
-                    continue
-                else:
-                    showlist.append(string)
+                showseq.append(string)
+                break
+            print(showseq)
+            ttt=child.find_all('a',{'data-linktype':'2'})
+#            for t in ttt:
+
+
+
             #print(showlist)
-            if(len(showlist)>3):
-                print(len(showlist))
-                print(showlist)
     #regex = re.compile("(>(\d{3,})\s{1,}< a href=(.*)\s{1,}target)")
     #print(regex.findall(t))
 
@@ -70,10 +71,10 @@ for t in ttt:
     link = t.get('href')
     print(link)
     link_href=' <a href="'+link
-    print(link_href)
-
-    patten = re.compile(r'[0-9]{2,5}(%s)(.*)' %link_href)
-    #    patten = re.compile(r'((/|.)*html)')
+    #     print(link_href)
+    # 
+    #     patten = re.compile(r'[0-9]{2,5}(%s)(.*)' %link_href)
+    #     #    patten = re.compile(r'((/|.)*html)')
     number=patten.search(html.unescape(htmls))
 #    print(patten)
     htmls = '<p style="font-size: 16px;line-height: 25.6000003814697px;white-space: normal;max-width: 100%;min-height: 1em;color: rgb(62, 62, 62);box-sizing: border-box !important;word-wrap: break-word !important;background-color: rgb(255, 255, 255);"><span style="max-width: 100%;white-space: pre-wrap;font-size: 14px;box-sizing: border-box !important;word-wrap: break-word !important;">001 <a href="http://mp.weixin.qq.com/s?__biz=MzIxMTMzNTc5OA==&amp;mid=2247483656&amp;idx=1&amp;sn=d7b68107b3e80d87974b02471a976b6c&amp;scene=21#wechat_redirect" target="_blank" data_ue_src="http://mp.weixin.qq.com/s?__biz=MzIxMTMzNTc5OA==&amp;mid=2247483656&amp;idx=1&amp;sn=d7b68107b3e80d87974b02471a976b6c&amp;scene=21#wechat_redirect" data-linktype="2">垫底辣妹</a>（嘉宾：冰心、龙源）</span></p>'
